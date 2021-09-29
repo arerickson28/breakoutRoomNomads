82503c9d16c456e32128702196db66c5176be94e
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-09-28 20:30:44 -0500

    rhyce beginning development on components for new poll creation

---
+++ client/src/components/newPoll/newPoll.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NewPoll() {
+    return (
+        <section>
+            <div>
+            <h1>Create New Poll!</h1>
+            </div>
+            <hr></hr>
+            
+      </section>
+    )
+}
